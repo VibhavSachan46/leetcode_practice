b30b3e7a24162627ec7aa083fb807f656c870cfa
Vibhav Raj Sachan <95123263+VibhavSachan46@users.noreply.github.com>
2021-12-26 12:24:10 +0530

    Time: 3 ms (99.77%), Space: 38.9 MB (88.87%) - LeetHub

---
+++ roman-to-integer/roman-to-integer.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int romanToInt(String s) {
+        int sum = 0;
+        int num=0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            switch (s.charAt(i)) {
+                case 'I':
+                    num = 1;
+                    break;
+                case 'V':
+                    num = 5;
+                    break;
+                case 'X':
+                    num = 10;
+                    break;
+                case 'L':
+                    num = 50;
+                    break;
+                case 'C':
+                    num = 100;
+                    break;
+                case 'D':
+                    num = 500;
+                    break;
+                case 'M':
+                    num = 1000;
+                    break;
+            }
+            if (num * 3 < sum)
+            {
+                sum = sum - num;
+
+            }
+            else
+            {
+                sum = sum + num;
+            }
+        }
+        return sum;
+    }
+}
